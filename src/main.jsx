5bb31faa5a234756854ed21f65a5b6985964894a
Ashikmahbub <ashikmahbub56@gmail.com>
2024-03-21 14:07:08 +0600

    login page added

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./router/Router";
 import { RouterProvider } from "react-router-dom";
-import {HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
+import Authprovider from "./components/Provider/Authprovider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div
-        className="max-w-screen-xl mx-auto bg-white
+    <Authprovider>
+      <HelmetProvider>
+        <div
+          className="max-w-screen-xl mx-auto bg-white
     
     
     "
-      >
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+        >
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </Authprovider>
   </React.StrictMode>
 );
