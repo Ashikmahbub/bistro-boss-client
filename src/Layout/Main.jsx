2ea0691d6542ce01c1691b4a47ba81adbce73827
Ashikmahbub <ashikmahbub56@gmail.com>
2024-04-29 12:28:46 +0600

    latest commit

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -7,7 +7,8 @@ import Navbar from "../shared/Navbar/Navbar";
 
 const Main = () => {
     const location = useLocation();
-    const noHeaderFooter = location.pathname.includes('login')
+    const noHeaderFooter = location.pathname.includes('login') ||
+    location.pathname.includes('signup');
     return (
         <div>
             {  noHeaderFooter || 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import { useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../components/Provider/Authprovider';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const captchaRef = useRef(null);
     const [disabled,setDisabled] =useState(true);
+    const {userSignIn} = useContext(AuthContext)
 
     useEffect(()=>{
       loadCaptchaEnginge(6);
@@ -25,16 +29,22 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email,password);
+        userSignIn(email,password)
+        .then(result =>{
+          const user = result.user;
+          console.log(user);
+        })
     }
     return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center md:w-1/2 lg:text-left">
+      <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            
           </div>
-          <div className="card md:w-1/2w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleLogin} className="card-body">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -57,9 +67,11 @@ const Login = () => {
                 <input  className="btn btn-primary" disabled={disabled} type="submit" value="Login" />
               </div>
             </form>
+            <p><small>New here ?<Link to='/signup'>Creat an account</Link></small> </p>
           </div>
         </div>
       </div>
+    </div> 
     );
 };
 
